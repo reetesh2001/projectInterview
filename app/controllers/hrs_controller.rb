eb9ee2e7726085c92d62a7ac2472cbb9c4d77cc4
Reetesh Kumar Pandey <rpandey@gammastack.com>
2024-07-19 14:48:25 +0530

    validation/update_delete_hr_opration

--- app/controllers/hrs_controller.rb
+++ app/controllers/hrs_controller.rb
@@ -1,10 +1,17 @@
 class HrsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:new, :create]
+  skip_before_action :authenticate_user!, only: [:new, :create]
 
   def show
     @hr = Hr.find_by(id: params[:id])
   end
+  def show
+    @hr = Hr.find_by(id: params[:id])
+  end
 
+  def new
+    @hr = Hr.new
+  end
   def new
     @hr = Hr.new
   end
@@ -13,12 +20,31 @@ class HrsController < ApplicationController
     @hr = Hr.new(hr_params)
     if @hr.save
       session[:user_id]=@hr.id
-      redirect_to @hr
+      redirect_to @hr, notice: 'User was successfully created.'
     else
-      render :new
+      render :new, status:422
     end
   end
 
+  def edit
+    @hr = Hr.find(params[:id])
+  end
+  
+  def update
+    @hr = Hr.find(params[:id])
+    if @hr.update(hr_params)
+      redirect_to @hr
+    else
+      render :edit
+    end
+  end
+    
+  def destroy
+    @hr = Hr.find(params[:id])
+    @hr.destroy
+    redirect_to root_path, status: :see_other
+  end
+  
   private
   def hr_params
     params.require(:hr).permit(:name, :email, :status, :password, :password_confirmation, :phone_number)
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,22 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:new, :create]
 
   def new
+    @hr = Hr.new
   end
   
   def create
     @hr = Hr.find_by(email: params[:email])
-    if @hr&.authenticate(params[:password])
+    if @hr != nil && @hr&.authenticate(params[:password])
       session[:user_id] = @hr.id
-      redirect_to @hr
+      redirect_to @hr, notice: 'User was successfully login.'
     else
-      render :new
+      flash[:alert] = 'User was not successfully login'
+      redirect_to login_path
     end
   end
   
   def destroy
     session[:user_id] = nil
-    redirect_to login_path
+    redirect_to login_path, notice: 'User was successfully logout.'
   end
 end
